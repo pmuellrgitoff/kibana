d6485593e966b595669d2cd5b8bfd1d4506ebaff
Patrick Mueller <pmuellr@gmail.com>
2020-10-02 18:24:59 -0400

    rename other ciGroup9 tests to ciGroup8

--- x-pack/test/ui_capabilities/spaces_only/tests/index.ts
+++ x-pack/test/ui_capabilities/spaces_only/tests/index.ts
@@ -13,7 +13,7 @@ export default function uiCapabilitesTests({ loadTestFile, getService }: FtrProv
   const featuresService: FeaturesService = getService('features');
 
   describe('ui capabilities', function () {
-    this.tags('ciGroup9');
+    this.tags('8iGroup8');
 
     before(async () => {
       // we're using a basic license, so if we want to disable all features, we have to ignore the valid licenses
